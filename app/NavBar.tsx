f96bde795413d1d6a38bc715f2b446c348804367
Jerry Hobby <jerry@jerryhobby.com>
2024-12-02 13:24:33 -0600

    AUTHENTICATING WITH NEXT AUTH / Access Sessions on the Client

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,16 +1,23 @@
+'use client';
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
+    const { status, data: session } = useSession();
+
     return (
         <div className='flex bg-slate-200 p-5 space-x-3'>
             <Link href='/'>NextJS</Link>
             <Link href='/users'>Users</Link>
-            <Link href='/api/auth/signin'>Login</Link>
-            <Link href={'/api/auth/signout'}>Logout</Link>
+            {status === "loading" && <div>Loading...</div>}
+            {status === "authenticated" && <div>{session.user!.name}</div>}
+            {status === "unauthenticated" && <Link href='/api/auth/signin'>Login</Link>}
+
             <Link href={'/api/auth/token'}>Token</Link>
         </div>
     )
 }
 
 export default NavBar
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-
-import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+
   return (
     <main>
       <h1>Hello World</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+      <div className="space-y-4">
+        <ProductCard />
+      </div>
     </main>
   );
 }
