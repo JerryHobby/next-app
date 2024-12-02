e671fc064f904b431abbc93c6e063123cb3ae7fc
Jerry Hobby <jerry@jerryhobby.com>
2024-12-02 15:55:13 -0600

    AUTHENTICATING WITH NEXT AUTH / Access Sessions on the Server

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import ProductCard from "./components/ProductCard";
+import { auth } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
 
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <div className="space-y-4">
         <ProductCard />
       </div>
