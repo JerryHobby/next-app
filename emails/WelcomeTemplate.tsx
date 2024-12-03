5708d509ec867ee271f3012b4cf325fb7d1c1cb9
Jerry Hobby <jerry@jerryhobby.com>
2024-12-03 15:14:01 -0600

    SENDING EMAILS / Styling Emails

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import { Html, Body, Container, Text, Link, Preview } from '@react-email/components'
+import React, { CSSProperties } from 'react'
+import {
+    Html,
+    Body,
+    Container,
+    Tailwind,
+    Text,
+    Link,
+    Preview
+} from '@react-email/components'
 
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
-        <Preview>Welcome Aboard!</Preview>
-        <Body>
+          <Preview>Welcome Aboard!</Preview>
+          <Tailwind>
+        <Body className='bg-white'>
             <Container>
-                  <Text>Hello {name}</Text>
+                  <Text className=' font-bold text-3xl'>Hello {name}</Text>
                   <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
             </Container>
-        </Body>
+              </Body>
+          </Tailwind>
     </Html>
   )
 }
