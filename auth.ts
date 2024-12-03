dabf90b36792612d26dac131fdb4e05a5a48d484
Jerry Hobby <jerry@jerryhobby.com>
2024-12-03 12:15:08 -0600

    AUTHENTICATING WITH NEXT AUTH / Configuring Credentials Providers

--- auth.ts
+++ auth.ts
@@ -1,10 +1,40 @@
 import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
 import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "./prisma/client"
+import bcrypt from "bcryptjs"
 
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "email" },
+        password: { label: "Password", type: "password", placeholder: "password" },
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password)
+          return null;
+        
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email as string
+          },
+        });
+
+        if (!user) return null;
+        if (!user.hashedPassword) return null;
+
+        const passwordMatch = await bcrypt.compare(
+          credentials.password as string,
+          user.hashedPassword as string);
+
+        return passwordMatch ? { id: user.id, email: user.email } : null;
+      },
+    }),
+    
+    Google],
 })
